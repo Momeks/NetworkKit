a06bcb845867662f05440ec4a9d821b8961e25e2
Momeks <momeksdev@gmail.com>
2025-05-06 12:12:41 +0200

    More general error message for users

--- Sources/NetworkKit/NetworkError.swift
+++ Sources/NetworkKit/NetworkError.swift
@@ -36,14 +36,8 @@ public enum NetworkError: Error, LocalizedError {
 public extension NetworkError {
     var userMessage: String {
         switch self {
-        case .invalidURL:
-            return "Invalid request. Please try again later."
-        case .requestFailed:
-            return "Failed to connect to the server. Please try again later."
-        case .decodingFailed:
-            return "We’re having trouble processing data. Please try again."
-        case .invalidResponse:
-            return "Unexpected response from the server."
+        case .invalidURL, .requestFailed, .decodingFailed, .invalidResponse:
+            return "Something went wrong. Please try again."
         case .connectionError:
             return "Please check your internet connection."
         case .unauthorized:
